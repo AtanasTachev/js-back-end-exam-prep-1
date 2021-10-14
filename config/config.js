4eecf413e3402476e244903cddcf71fdf0de38cf
Atanas Tachev <tachev_a#yahoo.com>
2021-10-14 15:29:49 +0300

    now running, no css though

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000,
-        // DB_CONNECTION: "mongodb://localhost:27017/real-estates"
+        port: process.env.PORT || 3000
     }
 }
